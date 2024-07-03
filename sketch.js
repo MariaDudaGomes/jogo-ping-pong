1b96727485a5583c4af224d803f8197ffd3102d5
MariaDudaGomes <maria.gomes.cruz@escola.pr.gov.br>
2024-07-03 08:35:35 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,49 @@
+let xBolinha =300;
+let yBolinha = 200;
+let velocidadeXBolinha= 10;
+let velocidadeYBolinha= 8;
+
+function setup() {
+  createCanvas (600, 400);
+}
+
+function draw() {
+  background(255, 156, 217);
+circle(xBolinha, yBolinha, 40);
+  xBolinha += velocidadeXBolinha;
+//  yBolinha+= velocidadeYBolinha;
+  
+  if (xBolinha >   xBolinha <0}
+  
+  if (xBolinha > width ||xBolinha <0   ){
+  velocidadeXBolinha *= -1;
+  
+}}
+
+
+
+
+
+
+
+}
+if (yBolinha +raio > height || yBolinha- raio <0)}
+   velocidadeYBolinha *=-1 
+
+
+
+if (Bolinha >width || yBolinha - raio >0)}
+ velocidadeyBolinha* -1;
+
+
+
+
+
+
+
+Função da minhs raquete 
+funtion minhaRaquete(){}
+rect(20, 150, 25,100)
+rec(MinhaRaquete,yMinhaRaquete, larguraMinhaRaquete, aluturaMinhaRaquete)
+}''
+if keylsDown DOWN_ARROW
